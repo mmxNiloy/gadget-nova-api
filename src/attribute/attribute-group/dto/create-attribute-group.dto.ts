0b1664bd8cd079031166be362d062148f4340dba
Shovon <sadiq.zaman@shohoz.com>
2025-03-22 03:15:35 +0600

    validation removed

--- src/attribute/attribute-group/dto/create-attribute-group.dto.ts
+++ src/attribute/attribute-group/dto/create-attribute-group.dto.ts
@@ -3,8 +3,8 @@ import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateAttributeGroupDto {
   @ApiProperty({ default: 'Screen Size' })
-  @IsNotEmpty({ message: 'Title must be non-empty' })
-  @IsString({ message: 'Title must be a string' })
-  @MaxLength(255, { message: 'Maximum 255 characters supported for title' })
+  // @IsNotEmpty({ message: 'Title must be non-empty' })
+  // @IsString({ message: 'Title must be a string' })
+  // @MaxLength(255, { message: 'Maximum 255 characters supported for title' })
   title: string;
 }
