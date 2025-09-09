38fd2a6607e5ae74407b9c838a7c460f56c58ebe
Niloy Sarkar <53993437+mmxNiloy@users.noreply.github.com>
2025-09-10 01:28:32 +0600

    Revert "Revert "Resync with the upstream""

--- src/search/search.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import {ElasticsearchService} from './elasticsearh.service'
-
-@Module({
-  providers: [ElasticsearchService],
-  exports: [ElasticsearchService],
-})
-export class SearchModule {}
